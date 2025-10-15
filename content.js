d14bd15c7a43bd2be714839dbf94d9ee59511f3c
Muneera jama <manhajama@Manhas-MacBook-Air.local>
2025-10-15 17:44:58 +0100

    update:scraper works with sales nav

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
 
   console.log('linkedIn scraper extension activated');
   try{
-    const rows = document.querySelectorAll('div.flex.flex-col.justify-center.items-start.ml-1.leading-regular');
+    const rows = document.querySelectorAll('.models-table-wrapper.background-color-white');
     if (!rows.length) {
         console.log('No profiles found on this page.');
         return '';
@@ -13,21 +13,43 @@
      const spans = [...row.querySelectorAll('span')].map(el => el.innerText.trim()).filter(Boolean);
      const divs = [...row.querySelectorAll('div')].map(el => el.innerText.trim()).filter(Boolean);
      
-     const name = spans[0].trim() ||'';
-     const degree = spans[1].trim() ||'';
+     let leadName = '';
+     let title = '';
 
-     const headline = divs[1]||'';
-     const location = spans[spans.length-1] ||'';
+     const nameBlock = row.querySelector('div');
+     if (nameBlock) { //grabbing the whole nameBlock
+      const nameSpans = [...nameBlock.querySelectorAll('span')];
+      const nameDivs = [...nameBlock.querySelectorAll('div')];
+//removing what i actually want
+      leadName = nameSpans[0]?.innerText?.trim() ||'';
+      title = nameDivs[1]?.innerText?.trim() ||'';
+//pushing at the end of the for loop
+     
+     }
+     const listCells = row.querySelectorAll('td[class^="list-people-detail-header"]');
+     const company = spans[3]?.trim() ||'';
 
-     data.push({name,degree,headline,location});
+     const location = listCells[2]?.innerText?.trim()||'';
+     const outreachActivity = listCells[4]?.innerText?.trim() ||''; 
+     const dateAdded = listCells[5]?.innerText?.trim() ||'';
+
+
+     data.push({
+      leadName,
+      title,
+      company,
+      location,
+      outreachActivity,
+      dateAdded});
     }
+
    console.log('Scraped profiles:', data);
    alert(`Extracted ${data.length} profiles. Check the console for details.`); //the safe stuff: DO NOT change
    
    //beginning CSV change
    const csvRows = [
-    ['Name','Degree', 'Headline', 'Location'],
-    ...data.map(d =>[d.name,d.degree,d.headline,d.location])
+    ['Name','Title', 'Company', 'Location', 'Outreach Activity','Date Added'],
+    ...data.map(d =>[d.leadName,d.title,d.company,d.location,d.outreachActivity,d.dateAdded])
    ] //array of arrays: first row is headers, rest are data rows, hence the double []
    .map(r => r.map(c=> `"${c.replace(/"/g,'""')}"`).join(','))//escape qoutes and join rows with commas
    .join('\n');
